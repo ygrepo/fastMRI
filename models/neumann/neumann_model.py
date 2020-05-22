cb9a2019f1833bfffe4969023113189abcbad0f7
Yves Greatti <yvgrotti@gmail.com>
2020-05-22 15:49:38 -0400

    Cleaned up code

--- models/neumann/neumann_model.py
+++ models/neumann/neumann_model.py
@@ -40,48 +40,6 @@ class NeumannNetwork(nn.Module):
 
         return runner_img_abs
 
-    def forward2(self, kspace):
-
-        runner_img, runner_img_abs = forward_adjoint_helper(self.device, self.hparams, self.mask_func, kspace,
-                                                            target=None)
-        runner_img_abs = runner_img_abs.unsqueeze(1)
-        # return self.reg_network(runner_img_abs.unsqueeze(1))
-
-        # runner_img_abs = runner_img_abs.unsqueeze(0).permute((1, 0, 2, 3))
-        XT_y = self.eta * runner_img_abs
-
-        # unrolled gradient iterations
-        for i in range(self.n_blocks):
-            # print(f"\nNNeumann Iteration:{i}")
-            linear_component = (1 - self.eta) * XT_y
-            learned_component = self.reg_network(XT_y)
-            # learned_component = torch.rfft(learned_component, 1, onesided=False).float().squeeze()
-
-            neumann_sum = linear_component - learned_component
-
-        return neumann_sum
-
-    # def forward3(self, kspace):
-    #
-    #     runner_img, runner_img_abs = forward_adjoint_helper(self.device, self.hparams, self.mask_func, kspace,
-    #                                                         target=None)
-    #     neumann_sum = runner_img
-    #
-    #     # unrolled gradient iterations
-    #     for i in range(self.n_blocks):
-    #         # print(f"\nNNeumann Iteration:{i}")
-    #         gramian_img, gramian_img_abs = gramian_helper(self.device, self.hparams, self.mask_func, runner_img)
-    #         linear_component = runner_img - self.eta * gramian_img
-    #         gramian_img_abs = gramian_img_abs.unsqueeze(0).permute((1, 0, 2, 3))
-    #         learned_component = -self.reg_network(gramian_img_abs)
-    #         learned_component = torch.rfft(learned_component, 1, onesided=False).float().squeeze()
-    #
-    #         runner_img = linear_component + learned_component
-    #         neumann_sum = neumann_sum + runner_img
-    #
-    #
-    #     return transforms.complex_abs(neumann_sum)
-
     def parameters(self):
         return list([self.eta]) + list(self.reg_network.parameters())
         # return list([self.eta, self.lambda_param]) + list(self.reg_network.parameters())
